5c8c56a35a5a41f234395d4262e3be48e165cb86
Mariela Luycho <170436@gmail.com>
2024-10-10 00:33:58 -0500

    Implementacion del dashboard inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: ()=> import('./componentes/login/login.component')
+    },
+    {
+        path: 'register',
+        loadComponent: ()=> import('./componentes/register/register.component')
+    },
+    {
+        path: 'forgot-password',
+        loadComponent: ()=> import('./componentes/forgot-password/forgot-password.component')
+    },
+    {
+        path: 'dashboard',
+        loadComponent: ()=> import('./componentes/dashboard/dashboard.component')
+    },
+];
---
+++ src/app/componentes/forgot-password/forgot-password.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DefaultTitleStrategy } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-password.component.html',
+  styleUrl: './forgot-password.component.css'
+})
+export default class ForgotPasswordComponent {
+
+}
